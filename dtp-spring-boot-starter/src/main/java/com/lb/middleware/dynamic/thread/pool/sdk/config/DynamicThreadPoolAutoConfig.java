dd0eb70af2141138a23db0489f4dce783f7365d4
LunerBreeze <RainSoul1120@outlook.com>
2024-08-29 13:19:38 +0800

    feat:搭建动态线程池组件工程

---
+++ dtp-spring-boot-starter/src/main/java/com/lb/middleware/dynamic/thread/pool/sdk/config/DynamicThreadPoolAutoConfig.java
@@ -0,0 +1,10 @@
+package com.lb.middleware.dynamic.thread.pool.sdk.config;
+
+/**
+ * 动态配置入口
+ * @author RainSoul
+ * @create 2024-08-29
+ */
+public class DynamicThreadPoolAutoConfig {
+
+}
---
+++ dtp-spring-boot-starter/src/test/java/com/lb/middleware/dynamic/thread/pool/ApiTest.java
@@ -0,0 +1,8 @@
+package com.lb.middleware.dynamic.thread.pool;
+
+/**
+ * @author RainSoul
+ * @create 2024-08-29
+ */
+public class ApiTest {
+}
